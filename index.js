588df05c3ce311c8c2e7b44d1289a73ac05ba73d
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2024-11-30 19:43:45 +0900

    examples from in workshop. embarrassing

--- index.js
+++ index.js
@@ -12,8 +12,67 @@ async function main() {
     await client.connect();
     console.log('Connected successfully');
     const db = client.db(dbName);
-    const collection = db.collection('employees');
-
+    const employees = db.collection('employees');
+    const teams = db.collection('teams');
+    let result = await teams.insertMany([
+        {
+            name: "Product Management"
+        },
+        {
+            name: "Frontend Engineering"
+        }
+    ]);
+    console.log(`inserted ids: ${result.insertedIds}`);
+    result = await employees.insertMany([
+        {
+            name: "Beege",
+            title: "Team Lead",
+            joined: "$date(2021-01-04)",
+            team: {
+                _id: result.insertedIds[0],
+                name: "Product Management"
+            }
+        },
+        {
+            name: "Andrei",
+            title: "Software Engineer",
+            joined: "$date(2021-06-01)",
+            team: {
+                _id: result.insertedIds[1],
+                name: "Frontend Engineering"
+            }
+        }
+    ]);
+    console.log(`inserted ids: ${result.insertedIds}`);
+    
+    let results = await employees.find(
+        {
+            "team.name": "Product Management"
+        }, 
+        {
+            projection: {name: 1, team: "$team.name", _id: 0}
+        }
+    ).toArray();
+    console.log(`find results: ${results}`);
+    console.log(await results);
+    results = await employees.aggregate([
+        {
+          '$match': {
+            'name': {
+              '$exists': 1
+            }
+          }
+        }, {
+          '$group': {
+            '_id': '$team.name', 
+            'countMembers': {
+              '$count': {}
+            }
+          }
+        }
+      ]).toArray();
+    console.log(`pipeline results: ${results}`);
+    console.log(results);
     return 'done';
 }
 
